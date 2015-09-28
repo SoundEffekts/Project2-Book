5954b1b0de67cb26cfde826f0fcdc1ecebd0095c
Chris Boardman <christophertboardman@gmail.com>
2015-09-28 01:24:51 -0400

    Finalized Lab #2
    Finished the 2nd Lab.

--- Book.java
+++ Book.java
@@ -13,66 +13,111 @@ class Book
     public String title;
     public int pages;
     public String refNumber;
+    public int borrowed;
+    public boolean courseText;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle, int bookPages)
+    public Book(String bookAuthor, String bookTitle, int bookPages, boolean course)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
         refNumber="";
+        course=courseText;
         
     }
     /**
-     * Returns the author of the book
-     * @return author
+     * For question 2.83
      */
     private String getAuthor()
     {
         return author;
     }
-    
+    /**
+     * For question 2.83
+     */   
     private String getTitle()
     {
         return title;
     }
-    
+    /**
+     * For question 2.91
+     * 
+    */      
+    private boolean getCourse()
+    {
+        return courseText;
+    }
+    /**
+     * For question 2.85
+     * 
+    */      
     private int getPages()
     {
         return pages;
     }  
-
+    /**
+     * For question 2.90
+     * 
+    */    
+    public void borrow()
+    {
+        borrowed=borrowed+1;
+    }  
+    /**
+     * For question 2.90
+     * 
+    */       
+    private int getBorrowed()
+    {
+        return borrowed;
+    }  
+    
     private String getRefNumber()
     {
         return refNumber;
     } 
-    
+    /**
+     * For question 2.89
+     * 
+    */        
     public void setRefNumber(String ref)
     {
-            if(refNumber.length()>2)
+            if(ref.length()>2)
+            {
                 refNumber=ref;
-            else(
-                System.out.println("ERROR: Reference number must be at least three characters."));
+            }
+            else{
+                System.out.println("ERROR: Reference number must be at least three characters.");   
+                }
+            
                 
                 
     }
+    /**
+     * For question 2.84
+     */
     private void printAuthor()
     {
         System.out.println("The author for this book is " + author);
     }
-    
+    /**
+     * For question 2.84
+    */    
     private void printTitle()
     {
         System.out.println("The title to this book is " + title);
     }
-    
+    /**
+     * For question 2.86, 2.88, 2.90
+    */      
     public void printDetails()
     {
         if (refNumber.length()>0)
-         System.out.println("Title: " + title + ". Author: " + author + ". Pages: " + pages + ". Reference Number: " + refNumber);
+         System.out.println("Title: " + title + ". Author: " + author + ". Pages: " + pages + ". Reference Number: " + refNumber + " This book has been borrowed " + borrowed + " times.");
        else
         System.out.println("ZZZ");
        
